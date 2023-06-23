16869f44533708a8da070f85a2feae3fc4632cc9
Florian Weber <florian@weber.world>
2023-06-23 16:34:48 +0200

    Allow specifying port via env variable PORT

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ async function bootstrap() {
     hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
     app.setViewEngine('hbs');
 
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
