db40d7aaf1a206076fe15b2d32c9ac53fc2bc0ec
Florian Weber <florian@weber.world>
2023-06-27 10:27:22 +0200

    Allow specifying global prefix via env var

--- src/main.js
+++ src/main.js
@@ -1,20 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { ConsoleLogger } from '@nestjs/common';
 import { join } from 'path';
+import _ from 'lodash';
 import hbs from 'hbs';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    const logger = new ConsoleLogger()
     const app = await NestFactory.create(AppModule);
 
-    app.setGlobalPrefix('/');
+    const port = process.env.PORT || 3000;
+    let globalPrefix = process.env.GLOBAL_PREFIX || '/';
 
-    app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/' });
+    logger.log(`Booting on port ${port}`)
+    if (globalPrefix !== '/') {
+        // Ensure we have a leading /, which is important for the `useStaticAssets` method used below
+        if (!_.startsWith('/', globalPrefix)) globalPrefix = '/' + globalPrefix;
+        logger.log(`Booting with global prefix "${globalPrefix}"`)
+    }
+
+    app.setGlobalPrefix(globalPrefix);
+
+    app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: globalPrefix });
     app.setBaseViewsDir(join(__dirname, '..', 'views'));
     hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
     app.setViewEngine('hbs');
 
-    await app.listen(process.env.PORT || 3000);
+    await app.listen(port);
 }
 
 bootstrap();
